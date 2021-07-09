0fcaca4cf16448b301a03e501f11df846227daff
AndrewMedvedev42 <sanjino56@gmail.com>
2021-07-09 13:24:26 +0300

    now weather data can be displayed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,10 +1,25 @@
+import React from 'react';
+import { useState } from "react";
+import {useDispatch} from "react-redux"
+import {loadWeather} from "../actions/weatherActoin"
+
 export const Nav = () => {
+    const dispatch = useDispatch()
+    const [cityName, setCityName] = useState("")
+
+    const onChange = (e) => {
+        setCityName(e.target.value);
+        console.log(e.target.value);
+    }
+    const submitAction = () => {
+        dispatch(loadWeather(cityName))
+    }
     return(
         <nav>
-            <form>
-                <input type="text" />
-                <button>Click</button>
-            </form>
+            <div>
+                <input onChange={onChange} type="text" />
+                <button value={cityName} type="submit" onClick={submitAction}>Click</button>
+            </div>
         </nav>
     )
 }
--- src/components/weatherDisplayer.jsx
+++ src/components/weatherDisplayer.jsx
@@ -1,8 +1,13 @@
+import React from 'react';
 import { useEffect } from "react"
 import {useDispatch, useSelector} from "react-redux"
 import {loadWeather} from "../actions/weatherActoin"
 
 export const WeatherDisplayer = () => {
+
+    const CurrentWeather = useSelector((state) => state.currentWeather)
+    const Forecast = useSelector((state) => state.weatherForecast)
+
     const dispatch = useDispatch()
 
     useEffect(()=>{
@@ -11,7 +16,23 @@ export const WeatherDisplayer = () => {
 
     return(
         <section>
-            <h1>Hello World</h1>
+            {CurrentWeather.data ? (
+                <section>
+                    <article>
+                        <h2>{CurrentWeather.data.location.country}</h2>
+                        <h1>{CurrentWeather.data.location.name}</h1>
+                        <h3>{CurrentWeather.data.location.localtime}</h3>
+                    </article>
+                    <article>
+                        <h2>{CurrentWeather.data.current.condition.text}</h2>
+                        <img src={CurrentWeather.data.current.condition.icon} alt="" />
+                    </article>
+                    <article>
+                        <p>Temp: {CurrentWeather.data.current.temp_c} C</p>
+                        <p>Wind: {CurrentWeather.data.current.wind_mph} C</p>
+                    </article>
+                </section>
+            ):console.log(null)}
         </section>
     )
 }
